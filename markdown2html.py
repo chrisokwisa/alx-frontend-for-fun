4bdfd6c0a54caa090658e7449afe76308db608e6
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 12:04:30 +0300

    Headings

--- markdown2html.py
+++ markdown2html.py
@@ -13,11 +13,27 @@ def markdown2html(input_file, output_file):
     except FileNotFoundError:
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+    lines = markdown_text.split("\n")
+    html_lines = []
 
-    html = markdown.markdown(markdown_text)
-
+    for line in lines:
+        if line.startswith("# "):
+            html_lines.append("<h1>" + line[2:] + "</h1>")
+        elif line.startswith("## "):
+            html_lines.append("<h2>" + line[3:] + "</h2>")
+        elif line.startswith("### "):
+            html_lines.append("<h3>" + line[4:] + "</h3>")
+        elif line.startswith("#### "):
+            html_lines.append("<h4>" + line[5:] + "</h4>")
+        elif line.startswith("##### "):
+            html_lines.append("<h5>" + line[6:] + "</h5>")
+        elif line.startswith("###### "):
+            html_lines.append("<h6>" + line[7:] + "</h6>")
+        else:
+            html_lines.append(line)
+    html_text = "\n".join(html_lines)
     with open(output_file, 'w') as f:
-        f.write(html)
+        f.write(html_text)
 
 
 if __name__ == '__main__':
