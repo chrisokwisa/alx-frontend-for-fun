4465f99a227bcb5c356b1150b2e58c3556228a41
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 08:50:31 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -19,9 +19,11 @@ def markdown2html(input_file, output_file):
     with open(output_file, 'w') as f:
         f.write(html)
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
     else:
         input_file = sys.argv[1]
