ac0427a99bdb25570c63f7f0e163ac828b4d4f70
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 12:08:14 +0300

    unorders listing

--- markdown2html.py
+++ markdown2html.py
@@ -17,20 +17,14 @@ def markdown2html(input_file, output_file):
     html_lines = []
 
     for line in lines:
-        if line.startswith("# "):
-            html_lines.append("<h1>" + line[2:] + "</h1>")
-        elif line.startswith("## "):
-            html_lines.append("<h2>" + line[3:] + "</h2>")
-        elif line.startswith("### "):
-            html_lines.append("<h3>" + line[4:] + "</h3>")
-        elif line.startswith("#### "):
-            html_lines.append("<h4>" + line[5:] + "</h4>")
-        elif line.startswith("##### "):
-            html_lines.append("<h5>" + line[6:] + "</h5>")
-        elif line.startswith("###### "):
-            html_lines.append("<h6>" + line[7:] + "</h6>")
+        if line.startswith("- "):
+            html_lines.append("<li>" + line[2:] + "</li>")
         else:
+            if html_lines[-1] != "<ul>":
+                html_lines.append("</ul>")
             html_lines.append(line)
+    if html_lines[-1] != "<ul>":
+        html_lines.append("</ul>")
     html_text = "\n".join(html_lines)
     with open(output_file, 'w') as f:
         f.write(html_text)
