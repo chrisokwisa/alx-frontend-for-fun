2f3fe0f1b04d1617d31dbf2e3220dd40bf0aedbb
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 22:09:56 +0300

    Bold and emphasis text

--- markdown2html.py
+++ markdown2html.py
@@ -13,56 +13,31 @@ def markdown2html(input_file, output_file):
     except FileNotFoundError:
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
+      # Initialize an empty list to store the lines of HTML
     html_lines = []
-    in_list = False
-    in_para = False
-    for line in lines:
-        if line.startswith("# "):
-            if in_para:
-                html_lines.append("</p>")
-                in_para = False
-            if in_list:
-                html_lines.append("</ol>")
-                in_list = False
-            html_lines.append("<h1>" + line[2:] + "</h1>")
-        elif line.startswith("## "):
-            if in_para:
-                html_lines.append("</p>")
-                in_para = False
-            if in_list:
-                html_lines.append("</ol>")
-                in_list = False
-            html_lines.append("<h2>" + line[3:] + "</h2>")
-        elif line.startswith("### "):
-            if in_para:
-                html_lines.append("</p>")
-                in_para = False
-            if in_list:
-                html_lines.append("</ol>")
-                in_list = False
-            html_lines.append("<h3>" + line[4:] + "</h3>")
-        elif line.startswith("#### "):
-            if in_para:
-                html_lines.append("</p>")
-                in_para = False
-            if in_list:
-                html_lines.append("</ol>")
-                in_list = False
-            html_lines.append("<h4>" + line[5:] + "</h4>")
-        elif line.startswith("##### "):
-            if in_para:
-                html_lines.append("</p>")
-                in_para = False
-            if in_list:
-                html_lines.append("</ol>")
-                in_list = False
-            html_lines.append("<h5>" + line[6:] + "</h5>")
-        elif line.startswith("1. ") or (in_list and line.startswith("  1. ")):
-            if not in_list:
-                html_lines.append("<ol>")
-                in_list = True
-            if line.startswith("  1. "):
-                line = line[4:]
+
+    # Split the markdown text into lines
+    for line in markdown_text.splitlines():
+        # If the line starts with '#', it's a heading
+        if line.startswith('#'):
+            level = line.count('#')
+            text = line.lstrip('#').lstrip()
+            html_lines.append(f"<h{level}>{text}</h{level}>")
+        # If the line starts with '-', it's a list item
+        elif line.startswith('-'):
+            text = line.lstrip('-').lstrip()
+            html_lines.append(f"<li>{text}</li>")
+        # Otherwise, it's a paragraph
+        else:
+            # check if the line contains bold syntax ** or __
+            line = line.replace("**", "<b>").replace("__", "<em>")
+            line = line.replace("**", "</b>").replace("__", "</em>")
+            html_lines.append(f"<p>{line}</p>")
+    # Join the list of lines into a single string of HTML
+    html_text = '\n'.join(html_lines)
+
+    with open(output_file, "w") as f:
+        f.write(html_text)
 
 
 if __name__ == '__main__':
