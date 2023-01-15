9aae029a6b103027225418f4f272ef6beacd3874
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 12:29:56 +0300

    ordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -2,48 +2,28 @@
 """ Has a script markdown2html """
 
 
+#!/usr/bin/python3
 import sys
 
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    sys.exit(1)
 
-def markdown2html(input_file, output_file):
-    """ Takes an argument 2 strings """
-    try:
-        with open(input_file, 'r') as f:
-            markdown_text = f.read()
-    except FileNotFoundError:
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-    lines = markdown_text.split("\n")
-    html_lines = []
-    in_list = False
-    for line in lines:
-        if line.startswith("# "):
-            html_lines.append("<h1>" + line[2:] + "</h1>")
-        elif line.startswith("## "):
-            html_lines.append("<h2>" + line[3:] + "</h2>")
-        elif line.startswith("### "):
-            html_lines.append("<h3>" + line[4:] + "</h3>")
-        elif line.startswith("#### "):
-            html_lines.append("<h4>" + line[5:] + "</h4>")
-        elif line.startswith("##### "):
-            html_lines.append("<h5>" + line[6:] + "</h5>")
-        elif line.startswith("* "):
-            if not in_list:
-                html_lines.append("<ol>")
-                in_list = True
-            html_lines.append("<li>" + line[2:] + "</li>")
-        else:
-            if in_list:
-                html_lines
+markdown_file = sys.argv[1]
+html_file = sys.argv[2]
 
+try:
+    with open(markdown_file, 'r') as f:
+        markdown_text = f.read()
+except FileNotFoundError:
+    print(f"Missing {markdown_file}", file=sys.stderr)
+    sys.exit(1)
 
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
-        sys.exit(1)
-    else:
-        input_file = sys.argv[1]
-        output_file = sys.argv[2]
-        markdown2html(input_file, output_file)
-        sys.exit(0)
+# Add ordered list extension to Markdown
+md = markdown.Markdown(extensions=['markdown.extensions.olist'])
+html = md.convert(markdown_text)
+
+with open(html_file, 'w') as f:
+    f.write(html)
+
+sys.exit(0)
