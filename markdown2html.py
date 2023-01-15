1e6eb7e49c7aa699248b1c451d4c33dadcac45a8
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 21:42:52 +0300

    Ordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -13,13 +13,56 @@ def markdown2html(input_file, output_file):
     except FileNotFoundError:
         print(f"Missing {input_file}", file=sys.stderr)
         sys.exit(1)
-
-    # Add ordered list extension to Markdown
-    md = markdown.Markdown(extensions=['markdown.extensions.olist'])
-    html = md.convert(markdown_text)
-
-    with open(output_file, 'w') as f:
-        f.write(html)
+    html_lines = []
+    in_list = False
+    in_para = False
+    for line in lines:
+        if line.startswith("# "):
+            if in_para:
+                html_lines.append("</p>")
+                in_para = False
+            if in_list:
+                html_lines.append("</ol>")
+                in_list = False
+            html_lines.append("<h1>" + line[2:] + "</h1>")
+        elif line.startswith("## "):
+            if in_para:
+                html_lines.append("</p>")
+                in_para = False
+            if in_list:
+                html_lines.append("</ol>")
+                in_list = False
+            html_lines.append("<h2>" + line[3:] + "</h2>")
+        elif line.startswith("### "):
+            if in_para:
+                html_lines.append("</p>")
+                in_para = False
+            if in_list:
+                html_lines.append("</ol>")
+                in_list = False
+            html_lines.append("<h3>" + line[4:] + "</h3>")
+        elif line.startswith("#### "):
+            if in_para:
+                html_lines.append("</p>")
+                in_para = False
+            if in_list:
+                html_lines.append("</ol>")
+                in_list = False
+            html_lines.append("<h4>" + line[5:] + "</h4>")
+        elif line.startswith("##### "):
+            if in_para:
+                html_lines.append("</p>")
+                in_para = False
+            if in_list:
+                html_lines.append("</ol>")
+                in_list = False
+            html_lines.append("<h5>" + line[6:] + "</h5>")
+        elif line.startswith("1. ") or (in_list and line.startswith("  1. ")):
+            if not in_list:
+                html_lines.append("<ol>")
+                in_list = True
+            if line.startswith("  1. "):
+                line = line[4:]
 
 
 if __name__ == '__main__':
