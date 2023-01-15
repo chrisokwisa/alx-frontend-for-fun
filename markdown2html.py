bcd020be102b4afe3886d3c5f673d597ad7de151
chrisokwisa <okwisachristabel@gmail.com>
2023-01-15 12:38:03 +0300

    Ordered listing

--- markdown2html.py
+++ markdown2html.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-""" Has a script markdown2html """
+""" Has a script that runs the markdown html """
 
 
-#!/usr/bin/python3
 import sys
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
 
-markdown_file = sys.argv[1]
-html_file = sys.argv[2]
+def markdown2html(input_file, output_file):
+    """ Takes an argument 2 strings """
+    try:
+        with open(input_file, 'r') as f:
+            markdown_text = f.read()
+    except FileNotFoundError:
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
 
-try:
-    with open(markdown_file, 'r') as f:
-        markdown_text = f.read()
-except FileNotFoundError:
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+    # Add ordered list extension to Markdown
+    md = markdown.Markdown(extensions=['markdown.extensions.olist'])
+    html = md.convert(markdown_text)
 
-# Add ordered list extension to Markdown
-md = markdown.Markdown(extensions=['markdown.extensions.olist'])
-html = md.convert(markdown_text)
+    with open(output_file, 'w') as f:
+        f.write(html)
 
-with open(html_file, 'w') as f:
-    f.write(html)
 
-sys.exit(0)
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+    else:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+        markdown2html(input_file, output_file)
+        sys.exit(0)
